5fd148a7fdacd3255a45e2508484412711c02b63
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-07-20 20:05:29 +0200

    add developer note

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,18 +4,25 @@ export default function Document() {
   return (
     <Html>
       <Head>
-     {/*    
-    This Website was built by Jonathan Weldon.
-    If you're interested in having a website built, contact me at jona.weldon@icloud.com or visit weldon.dk
-     */}
+        <meta
+          name="developer-note"
+          content="This Website was built by Jonathan Weldon.
+    If you're interested in having a website built, contact me at jona.weldon@icloud.com or visit weldon.dk"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
-          rel="stylesheet"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Cormorant+Infant:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
-<link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
-        
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Infant:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
